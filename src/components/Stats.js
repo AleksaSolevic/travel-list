8f00103b6af2cb6665bed07e644830e3b217bcc4
Aleksa2024 <solevic.aleksa@yahoo.com>
2025-04-02 16:56:19 +0200

    Work in progress

---
+++ src/components/Stats.js
@@ -0,0 +1,17 @@
+function Stats({ items }) {
+  if(!items.length) return <p className="stats">Start adding some items to your packing list 🏝️</p>;
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentPacked = Math.round((numPacked / numItems) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentPacked === 100 
+        ? "You got everything! Ready to go ✈️"
+          :`💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentPacked}%)`}
+      </em>
+    </footer>
+  )
+};
+ export default Stats;
